b8f8726615263dcede6a514290b74666bb067438
Mario Areias <mario@mx51.io>
2024-11-06 09:54:57 +1100

    updating alacritty config and replace zellij option

--- .config/rolling-alacritty.sh
+++ .config/rolling-alacritty.sh
@@ -4,7 +4,7 @@ SESSION_LIST=$(zellij list-sessions -s)
 TARGET="temp"
 
 ALACRITTY_HOME="/Applications/Alacritty.app/Contents/MacOS/"
-CREATE="alacritty -o 'window.position={ x = 1250, y = 0 }' -o 'window.dimensions={ columns=150, lines=10 }' -e zellij -l temp -s $TARGET &"
+CREATE="alacritty -o 'window.position={ x = 1250, y = 0 }' -o 'window.dimensions={ columns=150, lines=10 }' -e zellij -n temp -s $TARGET &"
 ATTACH="alacritty -o 'window.position={ x = 1250, y = 0 }' -o 'window.dimensions={ columns=150, lines=10 }' -e zellij attach $TARGET &"
 
 for session in $SESSION_LIST; do
